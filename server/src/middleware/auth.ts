0db17f5626f46b43b145921f6c46c85578df16b0
Goose_tG <jarredhampton@hotmail.com>
2025-03-25 20:32:33 -0400

    added jwt authentication

---
+++ server/src/middleware/auth.ts
@@ -0,0 +1,30 @@
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+
+interface JwtPayload {
+  username: string;
+}
+
+export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
+  // verify the token exists and add the user data to the request object
+  // Get the authoriazation header from the request
+  const authHeader = req.headers.authorization;
+
+  if (authHeader) {
+
+    const token = authHeader.split(' ')[1];
+
+    const secretKey = process.env.JWT_SECRET_KEY || '';
+
+    jwt.verify(token, secretKey, (err, user) => {
+      if (err) {
+        return res.sendStatus(403);
+      }
+
+      req.user = user as JwtPayload;
+      next();
+    });
+  } else {
+    res.sendStatus(401);
+  }
+};
