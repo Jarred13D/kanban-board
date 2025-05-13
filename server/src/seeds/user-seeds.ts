1e34b7bf05fa58815fb2d7974c421801fadb0be6
Goose_tG <jarredhampton@hotmail.com>
2025-05-13 18:52:31 -0400

    update: seeds

--- server/src/seeds/user-seeds.ts
+++ server/src/seeds/user-seeds.ts
@@ -5,6 +5,6 @@ export const seedUsers = async () => {
     { username: 'JollyGuru', password: 'password' },
     { username: 'SunnyScribe', password: 'password' },
     { username: 'RadiantComet', password: 'password' },
-    { username: 'mattm', password: 'abc123'}
+    { username: 'jarredh', password: 'testpass123'}
   ], { individualHooks: true });
 };
