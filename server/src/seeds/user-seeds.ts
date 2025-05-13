052ed6386352c8c8b91c3602b0c844c58af8f439
Goose_tG <jarredhampton@hotmail.com>
2025-05-13 18:55:45 -0400

    update: seeds

--- server/src/seeds/user-seeds.ts
+++ server/src/seeds/user-seeds.ts
@@ -2,9 +2,9 @@ import { User } from '../models/user.js';
 
 export const seedUsers = async () => {
   await User.bulkCreate([
-    { username: 'JollyGuru', password: 'password' },
-    { username: 'SunnyScribe', password: 'password' },
-    { username: 'RadiantComet', password: 'password' },
+    { username: 'JimmyJohns', password: 'password' },
+    { username: 'TacoBell', password: 'password' },
+    { username: 'Wendys', password: 'password' },
     { username: 'jarredh', password: 'testpass123'}
   ], { individualHooks: true });
 };
