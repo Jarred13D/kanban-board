dedf242c411750f8c3121ecd4799df4e1a282ab2
Goose_tG <jarredhampton@hotmail.com>
2025-05-13 18:09:59 -0400

    update: front-end and db

--- client/src/api/authAPI.tsx
+++ client/src/api/authAPI.tsx
@@ -2,28 +2,28 @@ import { UserLogin } from "../interfaces/UserLogin";
 
 const login = async (userInfo: UserLogin) => {
   // TODO: make a POST request to the login route
-
+  
   try {
-    const response = await fetch('/auth/login', {
-      method: 'POST',
+    const response = await fetch("/auth/login", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
+        // Accept: "application/json",
+        // credentials: "include",
       },
       body: JSON.stringify(userInfo),
     });
-
-    if (!response.ok) {
-      const errorData = await response.json();
-      throw new Error(`Error: ${errorData.message}`);
+    if (response.ok) {
+      const data = await response.json();
+      return data;
     }
+    throw new Error('login failed');
 
-    const data = await response.json();
-
-    return data;
   } catch (err) {
-    console.log('Error from user login: ', err);
-    return Promise.reject('Could not fetch suer info');
+    console.log("Error from user login: ", err);
+    return Promise.reject("Could not fetch user info");
   }
-}
+};
 
 export { login };
+// all good
--- client/src/utils/auth.ts
+++ client/src/utils/auth.ts
@@ -1,30 +1,54 @@
-import { JwtPayload, jwtDecode } from 'jwt-decode';
+import { JwtPayload, jwtDecode } from "jwt-decode";
 
 class AuthService {
   getProfile() {
     // TODO: return the decoded token
+    return jwtDecode<JwtPayload>(this.getToken());
   }
 
   loggedIn() {
     // TODO: return a value that indicates if the user is logged in
+    const token = this.getToken();
+    // return !!token && !this.isTokenExpired(token);
+    return token;
   }
-  
-  isTokenExpired(token: string) {
+
+  isTokenExpired(token: string): boolean {
     // TODO: return a value that indicates if the token is expired
+    try {
+      const decoded = jwtDecode<JwtPayload>(token);
+      if (decoded.exp && decoded.exp * 1000 < Date.now()) {
+        // convert exp to millisec since JWT exp is in seconds
+        localStorage.removeItem("id_token");
+        // return decoded.exp * 1000 < Date.now();
+        return true;
+      }
+      return false;
+    } catch (error) {
+      console.log("invalid token format:", error);
+      localStorage.removeItem("id_token");
+      return true;
+    }
   }
 
   getToken(): string {
     // TODO: return the token
+    return localStorage.getItem("id_token") || "";
   }
 
   login(idToken: string) {
     // TODO: set the token to localStorage
+    localStorage.setItem("id_token", idToken);
     // TODO: redirect to the home page
+    window.location.assign("/");
   }
 
   logout() {
     // TODO: remove the token from localStorage
+    localStorage.removeItem("id_token");
+    sessionStorage.clear();
     // TODO: redirect to the login page
+    window.location.assign("/");
   }
 }
 
