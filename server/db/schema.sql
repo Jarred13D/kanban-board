5174b1495b0d59f26474f6f88ffa1d68ae90edc5
Goose_tG <jarredhampton@hotmail.com>
2025-05-13 20:13:53 -0400

    update: schema, bcrypt issue

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -1,19 +1,5 @@
--- Drop tables if they exist
-DROP TABLE IF EXISTS tickets CASCADE;
-DROP TABLE IF EXISTS users CASCADE;
+-- DROP DATABASE
+DROP DATABASE IF EXISTS kanban_db;
 
--- Create users table
-CREATE TABLE users (
-  id SERIAL PRIMARY KEY,
-  username VARCHAR(255) UNIQUE NOT NULL,
-  password VARCHAR(255) NOT NULL
-);
-
--- Create tickets table
-CREATE TABLE tickets (
-  id SERIAL PRIMARY KEY,
-  name VARCHAR(255) NOT NULL,
-  status VARCHAR(50) NOT NULL,
-  description TEXT,
-  assignedUserId INTEGER REFERENCES users(id)
-);
+-- CREATE DATABASE
+CREATE DATABASE kanban_db;
